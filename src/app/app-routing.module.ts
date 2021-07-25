4e22879eed9f62d1712a22b634b3e622e33ee05f
syyriinee <syrine.bensalem@gmail.com>
2021-07-25 13:46:52 +0100

    Init project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AuthGuard } from './core/guards/auth.guard';
+
+const appRoutes: Routes = [
+    {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+
+    },
+    {
+        path: 'dashboard',
+        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'customers',
+        loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule),
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'users',
+        loadChildren: () => import('./users/users.module').then(m => m.UsersModule),
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'account',
+        loadChildren: () => import('./account/account.module').then(m => m.AccountModule),
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'about',
+        loadChildren: () => import('./about/about.module').then(m => m.AboutModule),
+        canActivate: [AuthGuard]
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [RouterModule],
+    providers: []
+})
+export class AppRoutingModule { }
